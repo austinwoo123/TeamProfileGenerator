b98ea02f0305de8966a4c2fe4a50e535affa7849
Austin W <austinwoo123@gmail.com>
2021-01-23 02:30:25 -0800

    add semi colon for the manager questions

--- app.js
+++ app.js
@@ -24,18 +24,16 @@ const managerQuestions = () => {
             type: "input",
             message: "What is the team manager's id?",
             name: "managerId"
-        }
+        },
         {
             type: "input",
             message: "what is the team manager's email?",
             name: "managerEmail",
-        }
+        },
         {
             type: "input",
             message: "What is the team manager's office number?",
             name: "officeNumber"
-
-
         }
 
 
